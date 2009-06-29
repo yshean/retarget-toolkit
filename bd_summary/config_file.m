1cf091f1669eff039527a5b7b41d4d12f0f63ed4
yiqun.hu <yiqun.hu@aeedd7dc-6096-11de-8dc1-e798e446b60a>
2009-06-29 03:06:11 +0000

    add support for arbitrary resizing ratio [w,h]

--- bd_summary/config_file.m
+++ bd_summary/config_file.m
@@ -4,9 +4,9 @@ img_ext = '.jpg';
 patch_size = [7,7];
 
 scaling_factor = 1/4;
-resize_gap = 0.95;
+resize_gap = [1,1];
 resize_increase_factor = 0.95;
-resize_target = 0.9;
+resize_target = [0.5,0.5];
 
 converge_thresh = 0;
 upsample_factor = 2^(0.25);
@@ -14,7 +14,7 @@ complete_weight = 1;
 cohere_weight = 1;
 
 % NN search radius
-R = 3;
+R = 1;
 
 data_folder = '../data/';
 result_folder = '../results/';
