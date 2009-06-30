5f26b26e3a5a3b6bfdf3f1caa7a34252ff8c6eb0
yiqun.hu <yiqun.hu@aeedd7dc-6096-11de-8dc1-e798e446b60a>
2009-06-30 09:21:37 +0000

    bug fix

--- bd_summary/bd_summary_2d.m
+++ bd_summary/bd_summary_2d.m
@@ -178,7 +178,7 @@ while (scaling_factor*upsample_factor<1)
         for i = 1 : length(img_files)
             new_targets{i} = zeros(size(targets{i}));
             target = zeros(size(targets{i}));
-            new_targets{i} = bidirect_update_image(target,source_patches{i},source_patches_weights{i},sources_match_array{i},target_patches{i},targets_match_array{i});
+            new_targets{i} = bidirect_update_2d(target,source_patches{i},source_patches_weights{i},sources_match_array{i},target_patches{i},targets_match_array{i});
         end
         
         old_diff = diff;
