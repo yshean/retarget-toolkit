ebbf5b374ac4dabf02b3d94fa708f56940f42e96
yiqun.hu <yiqun.hu@aeedd7dc-6096-11de-8dc1-e798e446b60a>
2009-07-30 02:51:07 +0000

    milestone of shift-map 2d

--- trunk/shift_map/shift_map_2d/shift_map_2d.cpp
+++ trunk/shift_map/shift_map_2d/shift_map_2d.cpp
@@ -33,49 +33,75 @@
 
 using namespace std;
 
-#define MAX_COST_VALUE 10000
-
 struct ForDataFn
 {
-	int numLab;
-	int *data;
+	Picture *src;
+	int *assignments;
+	imageSize previous_size;
+	imageSize target_size;
 };
 
 struct ForSmoothFn
 {
 	Picture *src;
 	Matrix *gradient;
-	int target_width;
-	int target_height;
+	int *assignments;
+	imageSize target_size;
+	imageSize previous_size;
 	float alpha;
 	float beta;
 };
 
-int dataFn(int p, int l, void *data)
+double dataFn(int p, int l, void *data)
 {
 	ForDataFn *myData = (ForDataFn *) data;
-	int numLab = myData->numLab;
+
+	int x = p % myData->target_size.width;
+	int y = p / myData->target_size.width;
+
+	int assign_idx = DownsamplingIndex(p,myData->target_size,
+										myData->previous_size,2);
+	int assignment = ceil((double)myData->assignments[assign_idx]*2);
+
+	double cost = 0.0;
+	if (myData->assignments[0]<0)
+	{
+		// pixel rearrangement: 
+		// keep the leftmost/rightmost columns
+		if (x==0 && l!=0)
+			cost = MAX_COST_VALUE;
+		if (x==myData->target_size.width-1 && 
+			l!=myData->src->GetWidth()-myData->target_size.width)
+			cost = MAX_COST_VALUE;
+	}
+	else
+	{
+		//printf("Calculating data term for pixel (%d,%d,%d) with label %d\n",x,y,t,l);
+		if (x+assignment+l-1<0 || x+assignment+l-1>=myData->src->GetWidth())
+		//if (abs(l-assignment)<=1)
+			cost = MAX_COST_VALUE;
+	}
+
 	
-	return( myData->data[p*numLab+l] );
+	return cost;
 }
 
-int ColorDiff(Picture *src, int x1, int y1, 
+double ColorDiff(Picture *src, int x1, int y1, 
 				 int x2, int y2, int l1, int l2)
 {
-	int diff = 0;
+	double diff = 0.0;
 	int width = src->GetWidth();
 	int height = src->GetHeight(); 
 	int x_offset = x2-x1;
 	int y_offset = y2-y1;
 
 	//printf("ColorDiff: pixels: (%d,%d) and (%d,%d) labels: %d and %d\n",x2,y2+l2,x1+x_offset,y1+l1+y_offset,l2,l1);
-	if (y2+l2>=0 && y2+l2<width && y1+l1+y_offset>=0 && y1+l1+y_offset<width &&
-		x1+x_offset>=0 && x1+x_offset<height)
+	if (x2+l2>=0 && x2+l2<width && x1+l1+x_offset>=0 && x1+l1+x_offset<width)
 	{
 		//printf("(%d,%d) color components: %d,%d,%d\n",src->GetPixel(y2+l2,x2).r,src->GetPixel(y2+l2,x2).g,src->GetPixel(y2+l2,x2).b);
-		diff += pow((double)src->GetPixel(y2+l2,x2).r-src->GetPixel(y1+l1+y_offset,x1+x_offset).r,2);
-		diff += pow((double)src->GetPixel(y2+l2,x2).g-src->GetPixel(y1+l1+y_offset,x1+x_offset).g,2);
-		diff += pow((double)src->GetPixel(y2+l2,x2).b-src->GetPixel(y1+l1+y_offset,x1+x_offset).b,2);
+		diff += pow((double)src->GetPixel(x2+l2,y2).r-src->GetPixel(x1+l1+x_offset,y1+y_offset).r,2);
+		diff += pow((double)src->GetPixel(x2+l2,y2).g-src->GetPixel(x1+l1+x_offset,y1+y_offset).g,2);
+		diff += pow((double)src->GetPixel(x2+l2,y2).b-src->GetPixel(x1+l1+x_offset,y1+y_offset).b,2);
 	} else
 	{
 		diff += MAX_COST_VALUE;
@@ -86,19 +112,18 @@ int ColorDiff(Picture *src, int x1, int y1,
 	return diff;
 }
 
-int GradientDiff(Matrix *gradient, int x1, int y1, 
+double GradientDiff(Matrix *gradient, int x1, int y1, 
 					int x2, int y2, int l1, int l2)
 {
-	int diff = 0;
+	double diff = 0.0;
 	int width = gradient->NumOfCols();
 	int height = gradient->NumOfRows();
 	int x_offset = x2-x1;
 	int y_offset = y2-y1;
 
-	if (y2+l2>0 && y2+l2<=width && y1+l1+y_offset>0 && y1+l1+y_offset<=width &&
-		x1+x_offset>0 && x1+x_offset<=height)
+	if (x2+l2>0 && x2+l2<=width && x1+l1+x_offset>0 && x1+l1+x_offset<=width)
 	{
-		diff += pow((gradient->Get(x2,y2+l2)-gradient->Get(x1+x_offset,y1+l1+y_offset)),2);
+		diff += pow((gradient->Get(y2,x2+l2)-gradient->Get(y1+y_offset,x1+l1+x_offset)),2);
 	} else
 	{	
 		diff += MAX_COST_VALUE;
@@ -107,23 +132,40 @@ int GradientDiff(Matrix *gradient, int x1, int y1,
 	return diff;
 }
 
-int smoothFn(int p1, int p2, int l1, int l2, void *data)
+double smoothFn(int p1, int p2, int l1, int l2, void *data)
 {
-	int cost = 0;
+	ForSmoothFn *myData = (ForSmoothFn *) data;
+	double cost = 0.0;
 	//printf("smoothFn: calculating image gradient...\n");
-	int width = ((ForSmoothFn*)data)->target_width;
-	int height = ((ForSmoothFn*)data)->target_height;
+	int width = myData->target_size.width;
+	int height = myData->target_size.height;
 	//printf("smoothFn: image width=%d and height=%d\n",width,height);
-	int y1 = p1 % width;
-	int x1 = (p1-y1)/width;
-	int y2 = p2 % width;
-	int x2 = (p2-y2)/width;
+	int x1 = p1 % width;
+	int y1 = p1 / width;
+	int assign_idx = DownsamplingIndex(p1,myData->target_size,myData->previous_size, 2);
+	int assignment1 = ceil((double)myData->assignments[assign_idx]*2);
+
+	int x2 = p2 % width;
+	int y2 = p2 / width;
+	assign_idx = DownsamplingIndex(p2,myData->target_size,myData->previous_size,2);
+	int assignment2 = ceil((double)myData->assignments[assign_idx]*2);
 	//printf("smoothFn: (%d,%d) and (%d,%d)\n",x1,y1,x2,y2);
 
 	if (abs(x1-x2)<=1 && abs(y1-y2)<=1)
-	{
-		cost += ((ForSmoothFn*)data)->alpha*ColorDiff(((ForSmoothFn*)data)->src, x1, y1, x2, y2, l1, l2);
-		cost += ((ForSmoothFn*)data)->beta*GradientDiff(((ForSmoothFn*)data)->gradient, x1+1, y1+1, x2+1, y2+1, l1, l2);
+	{		
+		if (myData->assignments[0]>=0)
+		{
+			cost += myData->alpha*ColorDiff(myData->src, x1, y1, x2, y2, 
+											assignment1+l1-1, assignment2+l2-1);
+			cost += myData->beta*GradientDiff(myData->gradient,x1+1, y1+1, x2+1, y2+1, 
+												assignment1+l1-1, assignment2+l2-1);
+		}
+		else
+		{
+			cost += myData->alpha*ColorDiff(myData->src, x1, y1, x2, y2, l1, l2);
+			cost += myData->beta*GradientDiff(myData->gradient, x1+1, y1+1, x2+1, y2+1, l1, l2);
+		}
+
 	}
 	else 
 	{
@@ -134,53 +176,7 @@ int smoothFn(int p1, int p2, int l1, int l2, void *data)
 	return cost;
 }
 
-int CalcPixelCost(Picture *src, int pixel, int label, 
-				  int width, int height, int assignment)
-{
-	int cost = MAX_COST_VALUE;
-	int col = pixel % width;
-	int row = (pixel-col)/width;
-
-	if (assignment<0)
-	{
-		// pixel rearrangement: 
-		// keep the leftmost/rightmost columns
-		if (col==0 && label==0)
-			cost = 0;
-		if (col==width-1 && label==src->GetWidth()-width)
-			cost = 0;
-	} else
-	{
-		if (abs(label-assignment)<=1)
-			cost = 0;
-	}
-
-	//printf("CalcPixelCost: (%d,%d,%d) cost=%d\n",row,col,label,cost);
-	return cost;
-}
-
-int *CalcDataCost(Picture *src, int width, int height, int num_labels, int *assignments)
-{
-	int num_pixels = width*height;
-	// first set up the array for data costs
-	int *data = new int[num_pixels*num_labels];
-	for ( int i = 0; i < num_pixels; i++ )
-	{
-		int y1 = floor((double)(i % width)/2);
-		int x1 = floor((double)((i-y1)/width)/2);
-
-		int assign_idx = y1+x1*floor((double)width/2);
-
-		for (int l = 0; l < num_labels; l++ )
-		{
-			data[i*num_labels+l] = CalcPixelCost(src,i,l,width,height,ceil((double)assignments[assign_idx]*2));
-		}
-	}
-
-	return data;
-}
-
-void SaveRetargetPicture(GCoptimizationGridGraph *gc, Picture *src,int width, int height, char *name)
+void SaveRetargetPicture(int *labels, Picture *src,int width, int height, char *name)
 {
 	Picture *result = new Picture(width,height);
 	result->SetName(name);
@@ -190,11 +186,11 @@ void SaveRetargetPicture(GCoptimizationGridGraph *gc, Picture *src,int width, in
 	for ( int  i = 0; i < width*height; i++ )
 	{
 		x = i % width;
-		y = i/width;
+		y = i / width;
 		//printf("SaveRetargetPicture: GetPixel(%d,%d)\n",x+gc->whatLabel(i),y);
-		pixel.r = src->GetPixel(x+gc->whatLabel(i),y).r;
-		pixel.g = src->GetPixel(x+gc->whatLabel(i),y).g;
-		pixel.b = src->GetPixel(x+gc->whatLabel(i),y).b;
+		pixel.r = src->GetPixel(x+labels[i],y).r;
+		pixel.g = src->GetPixel(x+labels[i],y).g;
+		pixel.b = src->GetPixel(x+labels[i],y).b;
 		result->SetPixel(x,y,pixel);
 	}
 
@@ -205,20 +201,23 @@ void SaveRetargetPicture(GCoptimizationGridGraph *gc, Picture *src,int width, in
 // in this version, set data and smoothness terms using arrays
 // grid neighborhood structure is assumed
 //
-int *GridGraph_GraphCut(Picture *src, int *data, int width,int height,
+int *GridGraph_GraphCut(Picture *src, int *assignments, imageSize &target_size, imageSize &previous_size,
 						int num_labels, float alpha, float beta, char *target_name)
 {
-	int num_pixels = width*height;
+	int num_pixels = target_size.width*target_size.height;
 	int *result = new int[num_pixels];   // stores result of optimization
 
 	try{
-		GCoptimizationGridGraph *gc = new GCoptimizationGridGraph(width,height,num_labels);
+		GCoptimizationGridGraph *gc = new GCoptimizationGridGraph(target_size.width,
+																  target_size.height,
+																  num_labels);
 
 		// set up the needed data to pass to function for the data costs
 		ForDataFn toDataFn;
-		toDataFn.data = data;
-		toDataFn.numLab = num_labels;
-
+		toDataFn.src = src;
+		toDataFn.assignments = assignments;
+		toDataFn.target_size = target_size;
+		toDataFn.previous_size = previous_size;
 		gc->setDataCost(&dataFn,&toDataFn);
 
 		// smoothness comes from function pointer
@@ -226,23 +225,35 @@ int *GridGraph_GraphCut(Picture *src, int *data, int width,int height,
 		toSmoothFn.src = src;
 		Matrix *gradient = Gradient(src);
 		toSmoothFn.gradient = gradient;
-		toSmoothFn.target_width = width;
-		toSmoothFn.target_height = height;
+		toSmoothFn.assignments = assignments;
+		toSmoothFn.previous_size = previous_size;
+		toSmoothFn.target_size = target_size;
 		toSmoothFn.alpha = alpha;
 		toSmoothFn.beta = beta;
 		gc->setSmoothCost(&smoothFn, &toSmoothFn);
 
-		printf("Before optimization energy is %d\n",gc->compute_energy());
-		gc->expansion(2);// run expansion for 2 iterations. For swap use gc->swap(num_iterations);
-		printf("After optimization energy is %d\n",gc->compute_energy());
+		printf("Before optimization energy is %f\n",gc->compute_energy());
+		gc->expansion(1);// run expansion for 2 iterations. For swap use gc->swap(num_iterations);
+		printf("After optimization energy is %f\n",gc->compute_energy());
 
+		int assign_idx, assignment;
 		for ( int  i = 0; i < num_pixels; i++ )
 		{
 			//printf("The optimal label for pixel %d is %d\n",i,gc->whatLabel(i));
-			result[i] = gc->whatLabel(i);
+			assign_idx = DownsamplingIndex(i,target_size,previous_size,2);
+			if (assign_idx>=0)
+				assignment = ceil((double)assignments[assign_idx]*2);
+			else
+				assignment = -1;
+
+			if (assignment<0)
+				result[i] = gc->whatLabel(i);
+			else
+				result[i] = max(assignment+gc->whatLabel(i)-1,0);			
 		}
 
-		SaveRetargetPicture(gc,src,width,height,target_name);
+		SaveRetargetPicture(result,src,target_size.width,
+							target_size.height,target_name);
 
 		delete gradient;
 		delete gc;
@@ -261,7 +272,8 @@ int main(int argc, char **argv)
 	pyramidType *gpyramid = NULL;
 	int width, height;
 	int num_pixels, num_labels;
-	int *data, *assignments;
+	int *assignments, *new_assignments;
+	imageSize target_size, previous_size;
 
 	if (argc<6)
 	{
@@ -290,35 +302,39 @@ int main(int argc, char **argv)
 
 			num_labels = width-ceil(width*atof(argv[4]))+1;
 			width = ceil(width*atof(argv[4]));
-			data = CalcDataCost(&(gpyramid->Images[i]),width,height,
-								num_labels,assignments);	
 
-			// smoothness and data costs are set up using functions
+			target_size.width = width;
+			target_size.height = height;
+			previous_size.width = 0;
+			previous_size.height = 0;	
+
+			// smoothness and data costs are set up using functions			
+			new_assignments = GridGraph_GraphCut(&(gpyramid->Images[i]),assignments,target_size,previous_size,
+													num_labels,atof(argv[2]),atof(argv[3]),argv[5]);
 			delete [] assignments;
-			assignments = GridGraph_GraphCut(&(gpyramid->Images[i]),data,width,height,
-											num_labels,atof(argv[2]),atof(argv[3]),argv[5]);
+			assignments = new_assignments;
 		} else
 		{
 			width = gpyramid->Images[i].GetWidth();
 			height = gpyramid->Images[i].GetHeight();
 			num_pixels = ceil(width*atof(argv[4]))*height;
 
-			num_labels = width-ceil(width*atof(argv[4]))+1;
+			num_labels = 3;//width-ceil(width*atof(argv[4]))+1;
 			width = ceil(width*atof(argv[4]));
-			data = CalcDataCost(&(gpyramid->Images[i]),width,height,
-								num_labels,assignments);
+			
+			previous_size = target_size;
+			target_size.width = width;
+			target_size.height = height;
 
 			// refinement of initial assignment
-			if (assignments)
-				delete [] assignments;
-			assignments = GridGraph_GraphCut(&(gpyramid->Images[i]),data,width,height,
-											num_labels,atof(argv[2]),atof(argv[3]),argv[5]);
-
+			new_assignments = GridGraph_GraphCut(&(gpyramid->Images[i]),assignments,target_size,previous_size,
+													num_labels,atof(argv[2]),atof(argv[3]),argv[5]);
+			delete [] assignments;
+			assignments = new_assignments;
 		}
 	}
 
 	delete [] assignments;
-	delete data;
 	delete input;
 	delete gpyramid;
 
