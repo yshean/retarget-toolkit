729aae5546150a7d5dad5807b2ec9eea2e87832b
kidphys <kidphys@aeedd7dc-6096-11de-8dc1-e798e446b60a>
2009-10-12 08:05:32 +0000

    Add: (unit tested) Matrix3D::SetIplImageX Matrix3D::SetIplImageZ Matrix3D::GetIplImageX Matrix3D::GetIplImageZ
    Matrix3DChar::Get3DScalar
    Matrix3DChar::Set3DScalar

--- VideoMontage/Matrix3D.cpp
+++ VideoMontage/Matrix3D.cpp
@@ -6,7 +6,17 @@ Matrix3D::Matrix3D(int width, int height, int length)
 {
 	_width = width;
 	_height = height;
+	_length = length;
 }
+
+Matrix3D::Matrix3D(int width, int height, int length, int channel)
+{
+	_width = width;
+	_height = height;
+	_length = length;
+	_channel = channel;
+}
+
 
 Matrix3D::~Matrix3D(void)
 {
@@ -20,88 +30,151 @@ bool Matrix3D::IsCoordinateInside(int x, int y, int z)
 	return ret;
 }
 
-char Get3DChar(Matrix3D* matrix, int x, int y, int z)
-{ 
-	if(matrix->IsCoordinateInside(x, y, z))
-	{
-		char* data = matrix->_data;
-		// move to frame z
-		data += z * matrix->_width * matrix->_height;
+//char Get3DChar(Matrix3D* matrix, int x, int y, int z)
+//{ 
+//	if(matrix->IsCoordinateInside(x, y, z))
+//	{
+//		char* data = matrix->_data;
+//		// move to frame z
+//		data += z * matrix->_width * matrix->_height;
+//
+//		// align to row
+//		data += y * matrix->_width;
+//		
+//		// align to col
+//		data += x;	
+//
+//		return *data;
+//	}
+//	else
+//		return 0;
+//}
+//
+//int Get3DInt(Matrix3D* matrix, int x, int y, int z)
+//{
+//	if(matrix->IsCoordinateInside(x, y, z))
+//	{
+//		int* data = (int*) matrix->_data;
+//		
+//		// move to frame z
+//		data += z * matrix->_width * matrix->_height;
+//
+//		// align to row
+//		data += y * matrix->_width;
+//		
+//		// align to col
+//		data += x;	
+//
+//		return *data;
+//	}else
+//		return 0;
+//	
+//}
+//
+//void Set3DChar(Matrix3D* matrix, int x, int y, int z, char value)
+//{
+//	if(matrix->IsCoordinateInside(x, y, z))
+//	{
+//		int* data = (int*) matrix->_data;
+//		
+//		// move to frame z
+//		data += z * matrix->_width * matrix->_height;
+//
+//		// align to row
+//		data += y * matrix->_width;
+//		
+//		// align to col
+//		data += x;	
+//
+//		*data = value;
+//	}
+//	// else don't set anything
+//}
+//
+//void Set3DInt(Matrix3D* matrix, int x, int y, int z, int value)
+//{
+//	if(matrix->IsCoordinateInside(x, y, z))
+//	{
+//		int* data = (int*) matrix->_data;
+//		
+//		// move to frame z
+//		data += z * matrix->_width * matrix->_height;
+//
+//		// align to row
+//		data += y * matrix->_width;
+//		
+//		// align to col
+//		data += x;	
+//
+//		*data = value;
+//	}
+//	// else do not set anything
+//}
+//
+//
+//
 
-		// align to row
-		data += y * matrix->_width;
-		
-		// align to col
-		data += x;	
 
-		return *data;
-	}
-	else
-		return 0;
-}
-
-int Get3DInt(Matrix3D* matrix, int x, int y, int z)
+void Matrix3D::GetIplImageZ(int z, IplImage *image)
 {
-	if(matrix->IsCoordinateInside(x, y, z))
+	if( z >= 0 && z < _length)
 	{
-		int* data = (int*) matrix->_data;
-		
-		// move to frame z
-		data += z * matrix->_width * matrix->_height;
-
-		// align to row
-		data += y * matrix->_width;
-		
-		// align to col
-		data += x;	
-
-		return *data;
-	}else
-		return 0;
-	
-}
-
-void Set3DChar(Matrix3D* matrix, int x, int y, int z, char value)
-{
-	if(matrix->IsCoordinateInside(x, y, z))
-	{
-		int* data = (int*) matrix->_data;
-		
-		// move to frame z
-		data += z * matrix->_width * matrix->_height;
-
-		// align to row
-		data += y * matrix->_width;
-		
-		// align to col
-		data += x;	
-
-		*data = value;
+		for(int x = 0; x < _width; x++)
+			for(int y = 0; y < _height; y++)
+			{
+				CvScalar value = Get3DScalar(x, y, z);
+				cvSet2D(image, y, x, value);
+			}
+			//cvCvtColor(image, image, CV_RGB);
 	}
-	// else don't set anything
 }
 
-void Set3DInt(Matrix3D* matrix, int x, int y, int z, int value)
+void Matrix3D::GetIplImageX(int x, IplImage *image)
 {
-	if(matrix->IsCoordinateInside(x, y, z))
+	if( x >= 0 && x < _width)
 	{
-		int* data = (int*) matrix->_data;
-		
-		// move to frame z
-		data += z * matrix->_width * matrix->_height;
-
-		// align to row
-		data += y * matrix->_width;
-		
-		// align to col
-		data += x;	
-
-		*data = value;
+		for(int z = 0; z < _length; x++)
+			for(int y = 0; y < _height; y++)
+			{
+				CvScalar value = Get3DScalar(x, y, z);
+				cvSet2D(image, z, y, value);
+			}
 	}
-	// else do not set anything
+}
+
+void Matrix3D::SetIplImageZ(int z, IplImage *image)
+{
+	if( z >= 0 && z < _length)
+	{
+		for(int x = 0; x < _width; x++)
+		{
+			for(int y = 0; y < _height; y++)
+			{
+				CvScalar value = cvGet2D(image, y, x);
+				Set3DScalar(x, y, z, value);
+			}
+		}
+	}
+}
+
+void Matrix3D::SetIplImageX(int x, IplImage *image)
+{	
+	if( x >= 0 && x < _width)
+	{
+		for(int z = 0; z < _length; x++)
+			for(int y = 0; y < _height; y++)
+			{
+				CvScalar value = cvGet2D(image, x, y);
+				Set3DScalar(x, y, z, value);
+			}
+	}
 }
 
 
+
+
+
+#pragma region Matrix3DChar
 
 
 double Matrix3DChar::Get3D(int x, int y, int z)
@@ -124,11 +197,61 @@ double Matrix3DChar::Get3D(int x, int y, int z)
 		return 0;
 }
 
-void Matrix3DChar::Set3D(int x, int y, int z, double value)
+
+CvScalar Matrix3DChar::Get3DScalar(int x, int y, int z)
+{
+	CvScalar value = cvScalar(0);
+	
+	unsigned char* data = (unsigned char*)_data;	 
+	//char* data = _data;
+	// move to frame z
+	data += z * _width * _height * _channel;
+
+	// align to row
+	data += y * _width * _channel;
+	
+	// align to col
+	data += x * _channel;	
+	
+	for(int i = 0; i < _channel; i++)
+	{
+		value.val[i] = (double)*data;		
+		data++;
+	}
+
+	return value;
+}
+
+void Matrix3DChar::Set3DScalar(int x, int y, int z, CvScalar value)
 {
 	if(this->IsCoordinateInside(x, y, z))
 	{
-		char* data = _data;
+		unsigned char* data = (unsigned char*)_data;	 
+		// move to frame z
+		data += z * _width * _height * _channel;
+
+		// align to row
+		data += y * _width * _channel;
+		
+		// align to col
+		data += x * _channel;	
+		
+		for(int i = 0; i < _channel; i++)
+		{
+			*data = (unsigned char)value.val[i];		
+			data++;
+		}
+	}
+}
+
+
+void Matrix3DChar::Set3D(int x, int y, int z, double value)
+{
+	//printf("X: %i, Y: %i, Z: %i \n", x, y, z);
+
+	if(this->IsCoordinateInside(x, y, z))
+	{
+		char* data = _data;	 
 		// move to frame z
 		data += z * _width * _height;
 
@@ -138,6 +261,9 @@ void Matrix3DChar::Set3D(int x, int y, int z, double value)
 		// align to col
 		data += x;	
 
-		*data = value;
+		*data = (char)value;		 
 	} 
 }
+
+
+#pragma endregion
--- VideoMontage/Matrix3D.h
+++ VideoMontage/Matrix3D.h
@@ -1,6 +1,6 @@
 #pragma once
 #include <cv.h>
- 
+
 
 #define MAT3D_8S 0
 #define MAT3D_16S 1
@@ -9,18 +9,29 @@
 class Matrix3D
 {
 public:
+	// default 1 channel
 	Matrix3D(int width, int height, int length);
+	Matrix3D(int width, int height, int length, int channel);
 	~Matrix3D(void);
 
 	int _width;
 	int _height;
 	int _length;
-	int _type;
+	int _channel;
 
 	// check whether the coordinate is inside the volume
 	bool IsCoordinateInside(int x, int y, int z);
 	virtual double Get3D(int x, int y, int z) = 0;
+	virtual CvScalar Get3DScalar(int x, int y, int z) = 0;
 	virtual void Set3D(int x, int y, int z, double value) = 0;
+	virtual void Set3DScalar(int x, int y, int z, CvScalar value) = 0;
+
+	virtual void GetIplImageZ(int z, IplImage* image);
+	virtual void GetIplImageX(int x, IplImage* image);
+
+	// fill a image to the matrix
+	virtual void SetIplImageZ(int z, IplImage* image);
+	virtual void SetIplImageX(int x, IplImage* image);
 
 	char* _data;	
 };
@@ -28,14 +39,22 @@ public:
 class Matrix3DChar : public Matrix3D
 {
 public:
+	// default 1 channel
 	Matrix3DChar(int width, int height, int length) : Matrix3D(width, height, length)
+	{	
+		_data = (char*)malloc(length * width * height * sizeof(char));
+	}
+
+	Matrix3DChar(int width, int height, int length, int channel) : Matrix3D(width, height, length, channel)
 	{
-		_data = (char*)malloc(length * sizeof(char));
+		_data = (char*)malloc(length * width * height * channel * sizeof(char));
 	}
 
 	~Matrix3DChar(void);
 
 	virtual double Get3D(int x, int y, int z);
+	virtual CvScalar Get3DScalar(int x, int y, int z);
 	virtual void Set3D(int x, int y, int z, double value);
+	virtual void Set3DScalar(int x, int y, int z, CvScalar value);
 };
 
