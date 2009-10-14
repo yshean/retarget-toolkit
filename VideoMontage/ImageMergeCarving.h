f236c7e406b2827564f02afa2b98e5578ca9febd
kidphys <kidphys@aeedd7dc-6096-11de-8dc1-e798e446b60a>
2009-10-14 09:32:10 +0000

    Added: - ImageMergeCarving::GetOverlapImages - TestOverlap
    Replace old implement when test function is ambiguous (TestImageMergeCarvingOverlap).
    
    From now unit test has only 1 interface: TestImageMergeCarving

--- VideoMontage/ImageMergeCarving.h
+++ VideoMontage/ImageMergeCarving.h
@@ -20,6 +20,17 @@ public:
 	// assuming that 2 images having the same height
 	// slide 2 images over to see with which seam the 2 can be combined
 	virtual IplImage* GetMergedImage(IplImage* image1, IplImage* image2, int overlap_size);
+
+public:
+	// PRIVATE: get overlap area between 2 images
+	// the overlap area of right image is shifted 1 pixel
+	// so they are align one pixel shifted with each other
+	// so 2 pixel which is neighbor (horizontally) will have the 
+	// same coordinate in each overlap images.
+	// ** NOTE ** 
+	// - overlap1 and overlap2 should be null
+	virtual void GetOverlapImages(IplImage* image1, IplImage* image2, 
+		IplImage** overlap1, IplImage** overlap2, int overlap_size);
 };
 
 
