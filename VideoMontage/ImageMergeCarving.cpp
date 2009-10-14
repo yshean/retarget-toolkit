19697e88da8eba285ac46fa7b16e04d096c0e73b
kidphys <kidphys@aeedd7dc-6096-11de-8dc1-e798e446b60a>
2009-10-14 09:12:17 +0000

    - Fixed link error can not find TestImageMergeCarvingShowOverlap - Overlap area is now shifted 1 pixel for easier comparison

--- VideoMontage/ImageMergeCarving.cpp
+++ VideoMontage/ImageMergeCarving.cpp
@@ -13,28 +13,46 @@ IplImage* ImageMergeCarving::GetMergedImage(IplImage* image1, IplImage* image2, 
 	IplImage* result;
 
 	// get overlap area	
+	// the overlap area of right image is shifted 1 pixel
+	// so they are align one pixel shifted with each other
+	// so 2 pixel which is neighbor (horizontally) will have the 
+	// same coordinate in each overlap images.
 	IplImage* overlap1 = extractor->GetRegion(image1, 
 		cvPoint(image1->width - overlap_size, 0), 
 		cvSize(overlap_size, image1->height));
 
 	IplImage* overlap2 = extractor->GetRegion(image2,
-		cvPoint(0, 0), 
+		cvPoint(1, 0), 
 		cvSize(overlap_size, image2->height));
 
-	cvNamedWindow("overlap1");
-	cvNamedWindow("overlap2");
-	while(1)
+	// get cost image when a pixel is chosen to be interface between 2 images
+	for(int x = 0; x < overlap_size; x++)
+		for(int y = 0; y < image1->height; y++)
 	{
-		cvShowImage("overlap1", overlap1);
-		cvShowImage("overlap2", overlap2);
-		cvWaitKey(100);
+		CvScalar pixel_1 = cvGet2D(overlap1, y, x);
+		CvScalar pixel_2 = cvGet2D(overlap2, y, x);
+		double sum = 0;
+		for(int i = 0; i < 4; i++)
+		{
+			sum += abs(pixel_1.val[i] - pixel_2.val[i]);
+		}
 	}
+
+
+	//cvNamedWindow("overlap1");
+	//cvNamedWindow("overlap2");
+	//while(1)
+	//{
+	//	cvShowImage("overlap1", overlap1);
+	//	cvShowImage("overlap2", overlap2);
+	//	cvWaitKey(100);
+	//}
 
 	//SeamCarving* carving = new SeamCarving();
 	return result;
 }
 
-void TestImageMergeCarvingShowOverlap()
+void TestImageMergeCarving()
 {
 	ImageMergeCarving* carving = new ImageMergeCarving();
 	RegionExtractor* extractor = new RegionExtractor();
