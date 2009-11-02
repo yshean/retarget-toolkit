a0e57edfd710fab60c712e8a4c11be7a57fe6be9
kidphys <kidphys@aeedd7dc-6096-11de-8dc1-e798e446b60a>
2009-11-02 02:42:52 +0000

    Added: - ColorConverter::ConvertRGB2GRAY

--- trunk/VideoMontage/ColorConverter.h
+++ trunk/VideoMontage/ColorConverter.h
@@ -10,4 +10,7 @@ public:
 
 	// take out frame by frame and convert from RGB to LUV space
 	void ConvertRGB2LUV(Matrix3D* input, Matrix3D* output);
+
+	void ConvertRGB2GRAY(Matrix3D* input, Matrix3D* output);
+ 
 };
