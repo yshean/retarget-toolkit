efc26b64057f9e5fe1c62175979b68b5fd342fb4
kidphys <kidphys@aeedd7dc-6096-11de-8dc1-e798e446b60a>
2009-11-02 02:42:52 +0000

    Added: - ColorConverter::ConvertRGB2GRAY

--- VideoMontage/ColorConverter.cpp
+++ VideoMontage/ColorConverter.cpp
@@ -21,3 +21,17 @@ void ColorConverter::ConvertRGB2LUV(Matrix3D* input, Matrix3D* output)
 		output->SetIplImageZ(z, image);
 	}
 }
+
+void ColorConverter::ConvertRGB2GRAY(Matrix3D *input, Matrix3D *output)
+{
+	int length = input->_length;
+	IplImage* image = cvCreateImage(cvSize(input->_width, input->_height), input->_type, input->_channel);
+	IplImage* gray_img = cvCreateImage(cvSize(input->_width, input->_height), input->_type, 1);
+	for(int z = 0; z < length; z++)
+	{
+		input->GetIplImageZ(z, image);
+		cvCvtColor(image, gray_img, CV_BGR2GRAY);
+		output->SetIplImageZ(z, gray_img);
+	}
+}
+ 
