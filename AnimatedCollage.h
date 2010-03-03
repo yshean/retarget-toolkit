ebb10582cc85d1a2036d606d59ebd9827ac8423c
kidphys <kidphys@aeedd7dc-6096-11de-8dc1-e798e446b60a>
2010-03-03 07:24:11 +0000

    add several function and test unit for animated layout working copy

--- AnimatedCollage.h
+++ AnimatedCollage.h
@@ -7,15 +7,15 @@ class AnimatedCollage
 {
 public:
 	// refactor the animation part to new class
-	AnimatedCollage(ShotInfo* ShotInfo, VideoSequence* sequence, 
-		VideoSequence* selectedFrame, vector<LayoutFrame*>* layoutList);
+	AnimatedCollage(ShotInfo* shotInfo, VideoSequence* sequence, 
+		VideoSequence* selectedFrames, vector<LayoutFrame*>* layoutList);
 	~AnimatedCollage(void);
 
 public:
 // private member set public, advanced user can change it, otherwise don't touch it
 	ShotInfo* _shotInfo; // shot/ subshot info
 	VideoSequence* _sequence; // original sequence
-	VideoSequence* _selectedFrame; // list of selected frame
+	VideoSequence* _selectedFrames; // list of selected frame
 	vector<LayoutFrame*>* _layoutList; // layoutFrame for each selected frame, means frame & layout must match
 
 // mouse event related variables
@@ -25,32 +25,31 @@ protected:
 	int _current_x;
 	int _current_y;
 
-	// current subShot, if changed, the sequence will be reset
+	// current LayoutIndex, if changed, sequence will be reset
+	int _layoutIndex;
+
+	// current subShot, for the sake of easy updating
 	SubShot* _subShot;
 
 	// current frame index to update, should be increased everytime updated
 	int _currentFrame;
 
-	// current LayoutFrame, for the shake of easy updating
+	// this flag is to stop/start animation
+	bool _isAnimated;
+
+	// current LayoutFrame, for the sake of easy updating
 	LayoutFrame* _currentLayoutFrame;
-	
-	//void UpdateCollage(VideoSequence* sequence,
-	//	ShotInfo* shotInfo, 
-	//	vector<LayoutFrame*>* layoutList, 
-	//	SubShot* subShot, int selectedLayout,
-	//	IplImage* collage,
-	//	int* currentFrameIndex);
+public:
+	// update collage
+	// this method will change the collage if necessary (i.e x and y change
+	void UpdateCollage(IplImage* collage, int x, int y);
+
 protected:
-// operating function
-	//void UpdateCollage(VideoSequence* sequence,
-	//	ShotInfo* shotInfo, 
-	//	vector<LayoutFrame*>* layoutList, 
-	//	SubShot* subShot, int selectedLayout,
-	//	IplImage* collage,
-	//	int* currentFrameIndex);
+	// *************** operating function  *************
  
-	// get clicked subshot given mouse position and list of layout list
-	SubShot* GetClickedSubShot(int x, int y);
+	// get clicked subshot given clicked layoutindex
+	// allow faster process for updating collage
+	SubShot* GetClickedSubShot(int layoutIndex);
 
 	// detect layout selected in a list of layout
 	// this index is match with selected frames, too
@@ -61,4 +60,8 @@ protected:
 
 	// return -1 if name of file not found in sequence
 	int GetFrameIndex(char* fileName);
+
+	// draw next frame to the collage, using internal information
+	// very private function, only use after subshot, current frame etc are confirmed
+	void DrawNextFrame(IplImage* collage);
 };
